5806037793dc7e6b5d3afecf45a016bca6b0cb65
eojysele <eojysele@yandex.ru>
2023-02-09 23:49:36 +0300

    change lualine config

--- lua/eojysele/plugins/nvim-lualine.lua
+++ lua/eojysele/plugins/nvim-lualine.lua
@@ -3,6 +3,25 @@ require("lualine").setup {
         theme = 'catppuccin',
         section_separators = '',
         component_separators = '',
-        ignore_focus = {'NvimTree',},
-  },
+        ignore_focus = { 'NvimTree', },
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = {
+            { 'filename',
+                path = 1,
+                symbols = {
+                    modified = '',
+                    readonly = '',
+                    unnamed = '',
+                    newfile = 'New File'
+
+                },
+            },
+        },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 }
