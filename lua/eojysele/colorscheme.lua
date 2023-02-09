6bda539a490d46fa191d5ae59bfb498e1dfd0e75
eojysele <eojysele@yandex.ru>
2023-02-10 00:32:51 +0300

    change singns

--- lua/eojysele/colorscheme.lua
+++ lua/eojysele/colorscheme.lua
@@ -7,6 +7,9 @@ require("catppuccin").setup({
     integrations = {
         treesitter = true,
         symbols_outline = true,
+        dap = {
+            enabled = true,
+        }
     },
 })
 
@@ -14,4 +17,11 @@ local status, _ = pcall(vim.cmd, "colorscheme catppuccin")
 if not status then
     print("Colorscheme not found!") -- print error if colorscheme not installed
     return
+end
+
+local signs = {Error = "", Warn = "", Hint = "", Info = ""}
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
 end
